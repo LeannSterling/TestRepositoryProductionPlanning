f500578ea7792d13cbb71768f219b8f2d92ad482
Leann Sterling <leann.sterling@hotmail.com>
2020-01-10 13:58:08 -0500

    Updated character controller, changed name and removed print

--- ProductionPlanningTestProject/Assets/Scripts/CharacterController.cs
+++ ProductionPlanningTestProject/Assets/Scripts/CharacterController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    public string name = "Leann";
+    public string name = "Gamer Gurl";
     public int health = 200;
 
     // Start is called before the first frame update
@@ -16,6 +16,6 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(name);
+
     }
 }
